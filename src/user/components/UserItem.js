3e5fb6f011fdc8ed4e104a903529c9eaae4b511c
Mike Dacanay <migd64@gmail.com>
2020-07-29 21:28:53 -0700

    created UserItem and UsersList component

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './UserItem.module.css';
+
+const UsersItem = props => {
+    return <h2>test</h2>
+};
+
+export default UsersItem;
---
+++ src/user/components/UsersList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import UserItem from './UserItem';
+import classes from './UsersList.module.css';
+
+const UsersList = props => {
+    if (props.items.length === 0) {
+        return (
+            <div className="center">
+                <h2>No users found.</h2>
+            </div>
+        )
+    }
+
+    return (<ul>
+        {
+            props.items.map(user => {
+                // <UserItem key={user.id} id={user.id}
+                //     image={user.image}
+                //     name={user.name}/>
+                <UserItem/>
+            })
+        }
+    </ul>)
+};
+
+export default UsersList;
