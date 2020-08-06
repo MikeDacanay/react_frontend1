c42b79fe9db86d862f4711d960fd09cd7baddb3f
Mike Dacanay <migd64@gmail.com>
2020-08-05 17:11:30 -0700

    started google maps, need to show the actual map

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal';
+import Map from '../../shared/components/UIElements/Map';
 
 import './PlaceItem.css';
 
@@ -26,7 +27,10 @@ const PlaceItem = props => {
                         Close
                     </Button>}>
                 <div className="map-container">
-                    <h2>THE MAP!</h2>
+                    <Map
+                        center={props.coordinates}
+                        zoom={16}
+                    />
                 </div>
             </Modal>
             <li className="place-item">
