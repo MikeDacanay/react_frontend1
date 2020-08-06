1427dd40c592d67e5c87307c7ec101b68dd7e890
Mike Dacanay <migd64@gmail.com>
2020-08-05 21:14:50 -0700

    basic validation are in newplace comp

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
-
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './NewPlace.css';
 
 const NewPlace = () => {
 
   return <form className="place-form">
-    
     <Input
       element='input'
       type='text'
-      label='Title'/>
+      label='Title'
+      validators={[
+        VALIDATOR_REQUIRE()
+      ]}
+      errorText='Please enter a valid text'/>
   </form>
 
 
